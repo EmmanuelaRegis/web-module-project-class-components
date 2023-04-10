93127d4a527c34911e36f8473b1170f02fb00332
Emmanuela Regis <emmanuelaregis0607@gmail.com>
2023-04-10 01:32:21 -0400

    i tried

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './TodoList';
+import Form from './Form';
 
-export default class App extends React.Component {
+
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      todos: [
+        {
+      task: 'Organize Garage',
+      id: 1528817077286, 
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    }
+      ]
+    }
+  } 
+
+  handleAdd = (task) => {
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+
+
+      this.setState({
+        ...this.state,
+        todos: [...this.state.todos, newTodo]
+      });
+    }
+  }
+
+   handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return ( todo.completed === false);
+      })
+    });
+  }
+
+   handleToggle = (clickedId) => {
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo=> {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+
+        return todo;
+      })
+    });
+  }
+
   render() {
-    return (
-      <div>
-        Todo App
-      </div>
-    )
+  const  {todos}  = this.state;
+
+   return(
+    <div>
+      <h1>Todos</h1>
+      <TodoList handleToggle = {this.handleToggle} todos={todos}/>
+      <Form handleAdd={this.handleAdd}/>
+      <button onClick = {this.handleClear}>Clear</button>
+    </div>
+   );
   }
 }
+
+
+export default App;  
